9bd58a9cc25561659838ea24c4022c36da9e804f
Rafał Białek <rafal.bialek@intive.com>
2020-04-13 20:44:32 +0200

    feat(lab2): basic routes + add primeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+
+const appRouters: Routes = [
+  { path: 'landing', component: LandingComponent },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  { path: 'product/:id', component: ProductComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ProductsComponent,
+    ProductComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRouters), ButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
